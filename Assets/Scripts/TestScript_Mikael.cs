6977acfd1a11051707b5976365e557ebf6575e1c
mbernau83 <mikael.bernau@gmail.com>
2021-03-25 10:50:31 +0100

    mikael_test_script
    Adding empty script to test git

---
+++ Assets/Scripts/TestScript_Mikael.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript_Mikael : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello Git World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
