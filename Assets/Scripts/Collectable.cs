af748c0da9b548318948b83eb332baa12c76edb9
Jessan92 <75431493+Jessan92@users.noreply.github.com>
2021-04-01 16:33:10 +0200

    Resources, stone, wood, food

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public static int stoneUnits = 0;
+    public static int woodUnits = 0;
+    public static int foodUnits = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+        if(other.tag == "Stone")
+        {
+            stoneUnits += 1;
+        }
+
+        if (other.tag == "Wood")
+        {
+            woodUnits += 1;
+        }
+
+        if (other.tag == "Food")
+        {
+            foodUnits += 1;
+        }
+
+        Debug.Log("Stones: " + stoneUnits + "   Wood: " + woodUnits + "   Food: " + foodUnits);
+    }
+}
