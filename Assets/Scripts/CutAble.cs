8360c3fe73a9afd570261985f36b3668ebec03d8
keve118 <75620975+keve118@users.noreply.github.com>
2021-04-10 01:26:29 +0200

    Integrated and improved Resource script
    - On collide you will gather woods
    - Trees drop the rest

---
+++ Assets/Scripts/CutAble.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutAble : MonoBehaviour
+{
+    GameObject thisWood;
+    public int woodHealth = 5;
+    public int mass = 10;
+    public int force = 9;
+    public GameObject rest=null;
+    public float timeFallen = 5f;
+    private bool isFallen = false;
+
+    private void Start()
+    {
+        thisWood = transform.parent.gameObject;
+    }
+
+    private void Update()
+    {
+        if (woodHealth <= 0 && isFallen == false)
+        {
+            Rigidbody rb = thisWood.AddComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            rb.mass = mass;
+            rb.AddForce(Vector3.forward*force, ForceMode.Impulse);
+            StartCoroutine(destroyBush());
+
+            isFallen = true;
+        }
+
+        IEnumerator destroyBush()
+        {
+           
+            yield return new WaitForSeconds(timeFallen);          
+            Destroy(thisWood);
+            Instantiate(rest, transform.position + new Vector3(0,1,0), transform.rotation);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperties : MonoBehaviour
+{
+    public static int amountWood = 0;
+    public static int amountStone = 0;
+    public static int amountFood = 0;
+
+}
