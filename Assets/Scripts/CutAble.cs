839c318dbf398ddb1344cae3249044389af81648
keve118 <75620975+keve118@users.noreply.github.com>
2021-04-13 12:22:28 +0200

    TEST

--- Assets/Scripts/CutAble.cs
+++ Assets/Scripts/CutAble.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CutAble : MonoBehaviour
 {
-    GameObject thisResource;
+    GameObject thisWood;
     public int woodHealth = 5;
     public int mass = 10;
     public int force = 9;
@@ -14,17 +14,21 @@ public class CutAble : MonoBehaviour
 
     private void Start()
     {
+<<<<<<< Updated upstream
+        thisWood = transform.parent.gameObject;
+=======
         if(gameObject.tag=="Stone")
-            thisResource = gameObject;
+         thisResource = gameObject;
         if(gameObject.tag=="Wood")
             thisResource = transform.parent.gameObject;
+>>>>>>> Stashed changes
     }
 
     private void Update()
     {
         if (woodHealth <= 0 && isFallen == false)
         {
-            Rigidbody rb = thisResource.AddComponent<Rigidbody>();
+            Rigidbody rb = thisWood.AddComponent<Rigidbody>();
             rb.isKinematic = false;
             rb.useGravity = true;
             rb.mass = mass;
@@ -38,7 +42,7 @@ public class CutAble : MonoBehaviour
         {
            
             yield return new WaitForSeconds(timeFallen);          
-            Destroy(thisResource);
+            Destroy(thisWood);
             Instantiate(rest, transform.position + new Vector3(0,1,0), transform.rotation);
         }
 
