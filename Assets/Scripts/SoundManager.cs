ddc4bd546214d4d73b2b3e99ef92677b4ade9c95
Jessan92 <75431493+Jessan92@users.noreply.github.com>
2021-04-14 11:40:42 +0200

    Soundeffects footsteps

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -28,6 +28,12 @@ public class SoundManager : MonoBehaviour
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+       
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
         s.source.Play();
     }
 }
