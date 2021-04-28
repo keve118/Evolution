efd746b7b309d35fad8cc3de0782f99aaebea103
mbernau83 <mikael.bernau@gmail.com>
2021-04-28 13:22:55 +0200

    Pause menu
    Pause menu

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] private bool isPaused;
+
+    [Header("UI Game State Screens")]
+    //[SerializeField] private GameObject start;
+    [SerializeField] private GameObject pause;
+    //[SerializeField] private GameObject gameover;
+
+    void Update()
+    {
+        //TODO implement Gameover
+
+        if (!isPaused && Input.GetKeyDown("p"))
+        {
+            PauseGame();
+        }
+
+        else if (isPaused && Input.GetKeyDown("p"))
+        {
+            ResumeGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        StopGame();
+        Cursor.visible = true;
+        pause.SetActive(true);
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        StartGame();
+        Cursor.visible = false;
+        pause.SetActive(false);
+        isPaused = false;
+    }
+
+    public void StopGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+    }
+}
