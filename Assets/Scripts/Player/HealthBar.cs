67251b5e0c27cd0e0570b00a2aaf12aac23ee415
Jessan92 <75431493+Jessan92@users.noreply.github.com>
2021-05-05 15:36:00 +0200

    Scripts

---
+++ Assets/Scripts/Player/HealthBar.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthBar;
+    private float maxHealth = 100f;
+    private float currentHealth;
+
+    [SerializeField] private float amountOfHealthToAdd = 10f;
+    [SerializeField] private float amountOfHealthToLoose = 0.01f;
+
+    public static HealthBar instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && PlayerProperties.amountFood > 0)
+        {
+            AddHealth(amountOfHealthToAdd);
+        }
+
+        if (currentHealth > 0)
+            LooseHealth(amountOfHealthToLoose);
+    }
+
+    public void AddHealth(float amount)
+    {
+        if (currentHealth < maxHealth)
+        {
+            currentHealth += amount;
+            healthBar.value = currentHealth;
+            PlayerProperties.amountFood--;
+        }
+
+    }
+
+    public void LooseHealth(float amount)
+    {
+        currentHealth -= amount;
+        healthBar.value = currentHealth;
+    }
+
+}
