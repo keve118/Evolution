8147de1eff027694320aad81a4776efd12202377
mbernau83 <mikael.bernau@gmail.com>
2021-05-05 12:20:56 +0200

    Revert "Pause menu update"
    This reverts commit b1cb0bd847dda485121be08f83e3f4bdcae7f390.

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour
 {
