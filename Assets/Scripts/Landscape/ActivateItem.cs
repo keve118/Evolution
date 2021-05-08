f8f43538f52ae7f038eeb1bb1f92e5f2fc8ea098
keve118 <75620975+keve118@users.noreply.github.com>
2021-05-08 17:36:02 +0200

    GrassUpdateInProgress

---
+++ Assets/Scripts/Landscape/ActivateItem.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateItem : MonoBehaviour
+{
+    private Transform cameraPosition; 
+    private bool visible = true;
+    public float distanceToAppear = 1;
+    Renderer objRenderer;
+    private bool skipFunction=false;
+
+    private void Start()
+    {
+
+        cameraPosition = Camera.main.transform;
+        objRenderer = gameObject.GetComponentInChildren<Renderer>();
+    }
+
+    private void Update()
+    {
+        Checker();
+    }
+
+
+    //void OnBecameVisible()
+    //{
+    //    enabled = true;
+    //}
+    //void OnBecameInvisible()
+    //{
+    //    enabled = false;
+    //}
+    //private void OnGUI()
+    //{
+        
+    //}
+
+    private void Checker()
+    {
+        float distance = Vector3.Distance(cameraPosition.position, transform.position);
+
+        if (distance < distanceToAppear)
+        {
+            if (!visible)
+            {
+                objRenderer.enabled = true; // Show Object
+                visible = true;
+            }
+        }
+        else if (visible)
+        {
+            objRenderer.enabled = false; // Hide Object
+            visible = false;
+        }
+    }
+
+}
+
+
