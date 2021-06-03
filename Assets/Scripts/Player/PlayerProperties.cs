d53b84cbff091158f3268688b791e8cb0aed02a9
keve118 <75620975+keve118@users.noreply.github.com>
2021-06-03 18:48:55 +0200

    PropertiesUpdate

--- Assets/Scripts/Player/PlayerProperties.cs
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -31,6 +31,6 @@ public class PlayerProperties : MonoBehaviour
     {
         rayCastTransform = rayCastObject.transform;
         rayCastOrigin = rayCastTransform.position;         
-    }
+    }+
 
 }
